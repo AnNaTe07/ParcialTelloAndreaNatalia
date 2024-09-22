a2b975745099fdec56c1ae0c5e366f45c63d8829
AnNaTe07 <102766959+AnNaTe07@users.noreply.github.com>
2024-09-21 22:43:28 -0300

    correccion

--- app/src/main/java/com/softannate/parcialtelloandreanatalia/MainActivity.java
+++ app/src/main/java/com/softannate/parcialtelloandreanatalia/MainActivity.java
@@ -32,9 +32,9 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .setAnchorView(R.id.fab).show();
+          //      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+          //              .setAction("Action", null)
+          //              .setAnchorView(R.id.fab).show();
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
